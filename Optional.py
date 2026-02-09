a61e7b69bb4dabfd34c3799fcb9d12768fbb84bb
iyyneswaran <pdee5660@gmail.com>
2026-02-09 20:21:45 +0530

    Initial

---
+++ Optional.py
@@ -0,0 +1,10 @@
+from typing import Optional
+
+def say_hi(name: Optional[str] = None):
+    if name is not None:
+        print(f"Hey {name}!")
+    else:
+        print("Hello World")
+
+say_hi() # Output: Hello World
+say_hi("Iyynes") # Output: Iyynes
