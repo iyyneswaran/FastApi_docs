ace2d6737dece5278640c6868e24a27aa94219ec
iyyneswaran <pdee5660@gmail.com>
2026-02-10 21:47:33 +0530

    class

---
+++ class.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, year, department):
+        self.name = name
+        self.year = year
+        self.department = department
+    
+    def display(self):
+        print(self.name, "pursuing", self.year, "Engineering and currently at", self.year, "year")
+
+s1 = Student("Iyynes", "3rd", "Mechanical")
+s1.display()
